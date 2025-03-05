4a899a5c202769264c6000d01830d6f803d37069
Justin B <justin@penguintech.io>
2025-03-05 04:06:38 +0000

    started merger

--- libs/botClasses.py
+++ libs/botClasses.py
@@ -232,3 +232,30 @@ class message_output:
 @dataclass
 class data_output:
     data: list
+
+
+@dataclass
+class gateway():
+    gateway_type: str
+    gateway_server: str
+    channel_id: str
+    is_active: bool
+
+@dataclass
+class identityPayload():
+    identity_name: str
+
+@dataclass
+class activity():
+    name: str
+    amount: int
+
+@dataclass
+class contextPayload():
+    userid: int
+    activity: str
+    amount: int
+    text: str
+    namespace: str
+    namespaceid: int
+    platform: str
